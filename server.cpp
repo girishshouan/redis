23168b5894ea36878ca870ac0d055aee5b0c467e
girishshouan <girishshouan@gmail.com>
2023-12-28 17:21:16 +0000

    Added changes for protocol and to process one request at a time.

--- server.cpp
+++ server.cpp
@@ -9,7 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
-
+const size_t k_max_msg = 4096;
 
 static void msg(const char* msg) {
     fprintf(stderr, "%s\n", msg);
@@ -32,6 +32,73 @@ static void do_something(int connfd) {
 
     char wbuf[] = "world";
     write(connfd, wbuf, strlen(wbuf));
+}
+
+
+// ssize_t is used when the returm value can either be equal to the size or a negative value
+static int32_t read_full(int fd, char* buf, size_t n){
+    while(n > 0){
+        ssize_t rv = read(fd, buf, n);
+        if(rv <= 1){
+            return -1;
+        }
+
+        //assert((size_t)rv <= n);
+        n = n - (size_t)rv;
+        buf = buf + rv;
+    }
+    return 0;
+}
+
+static int32_t write_full(int fd, char * buf, size_t n){
+    while(n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if(rv <= 1){
+            return -1;
+        }
+
+        n = n - (size_t)rv;
+        buf = buf + rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd){
+    // First read the 4-byte header
+    char rbuf[4 + k_max_msg + 1];
+    int err = read_full(connfd, rbuf, 4);
+    errno = 0;
+    if(err){
+        printf("%s", strerror(errno));
+        return err;
+    }
+
+    //Not too sure about this
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    //Read request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    //Reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_full(connfd, wbuf, 4+len);
 }
 
 int main() {
@@ -60,6 +127,27 @@ int main() {
         die("listen()");
     }
 
+    /*
+    while (true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t socklen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+        if (connfd < 0) {
+            continue;   // error
+        }
+
+        // only serves one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
+        close(connfd);
+    }
+    */
+
     while (true) {
         // accept
         struct sockaddr_in client_addr = {};
@@ -69,7 +157,12 @@ int main() {
             continue;   // error
         }
 
-        do_something(connfd);
+        while(true){
+            int32_t err = one_request(connfd);
+            if(err){
+                break;
+            }
+        }
         close(connfd);
     }
 
